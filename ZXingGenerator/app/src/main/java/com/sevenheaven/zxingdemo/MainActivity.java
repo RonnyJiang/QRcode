259c5cd953f18c51aafb968e0f6b6912a687e357
ronny <ronny@innk.cn>
2017-12-17 21:52:38 +0800

    add qrcode generator module

---
+++ ZXingGenerator/app/src/main/java/com/sevenheaven/zxingdemo/MainActivity.java
@@ -0,0 +1,42 @@
+package com.sevenheaven.zxingdemo;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView qrcode1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        qrcode1 = (ImageView) findViewById(R.id.qrcode1);
+//        qrcode1.setImageBitmap(QRCode.createQRCode("1234567890123456789012345678901234567890" +
+//                "12345678901234567890123456789012345678901234567890123456789012345678901234567890" +
+//                "12345678901234567890123456789012345678901234567890123456789012345678901234567890" +
+//                "1234567890123456789012345678901234567890" +
+//                "12345678901234567890123456789012345678901234567890123456789012345678901234567890" +
+//                "12345678901234567890123456789012345678901234567890123456789012345678901234567890"));
+        qrcode1.setImageBitmap(QRCode.createQRCode("http://www.china-liantong.com"));
+    }
+
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+}
